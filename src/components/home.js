49df0e020e7c0a624090c989bd826af968099fcb
Gopika g <gopikag@xminds.com>
2024-06-14 14:40:56 +0530

    intal commit

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,6 @@
-import { Box, Typography, Grid } from "@mui/material";
-import React, { useEffect, useState, useRef } from "react";
+import { Box, Typography, Grid, Avatar } from "@mui/material";
+import React, { useEffect, useState } from "react";
 import img26 from "../images/img26.png";
-
 
 import img3 from "../images/img3.png";
 import img4 from "../images/img4.png";
@@ -21,20 +20,38 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpotify } from "@fortawesome/free-brands-svg-icons";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+
+function srcset(image, size, rows = 1, cols = 1) {
+  return {
+    src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
+    srcSet: `${image}?w=${size * cols}&h=${
+      size * rows
+    }&fit=crop&auto=format&dpr=2 2x`,
+  };
+}
+
 export default function Home() {
-  const photos1 = [img10,img9, img3, img4,img5, img6, img7, img8, img10, img11];
+  const photos1 = [
+    img10,
+    img9,
+    img3,
+    img4,
+    img5,
+    img6,
+    img7,
+    img8,
+    img10,
+    img11,
+  ];
 
   const [currentPhotoIndex1, setCurrentPhotoIndex1] = useState(0);
-  
-
-  const aboutRef = useRef(null);
-
-
-  const galleryRef = useRef(null);
-
-
-  const placesRef = useRef(null);
-
 
   const openInsta = () => {
     window.open(
@@ -53,7 +70,15 @@ export default function Home() {
       "_blank"
     );
   };
-
+  const [open, setOpen] = React.useState(false);
+const [dailogeTitle,setDailogeTitle]=useState()
+  const handleClickOpen = (label) => {
+    setDailogeTitle(label)
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
   useEffect(() => {
     const intervalId1 = setInterval(() => {
       setCurrentPhotoIndex1((prevIndex) => (prevIndex + 1) % photos1.length);
@@ -62,8 +87,7 @@ export default function Home() {
     return () => clearInterval(intervalId1);
     // eslint-disable-next-line
   }, []);
- 
- 
+
   return (
     <Grid>
       <Box
@@ -118,7 +142,6 @@ export default function Home() {
       <Grid
         container
         spacing={3}
-        ref={aboutRef}
         sx={{
           display: "flex",
           justifyContent: "center",
@@ -146,7 +169,7 @@ export default function Home() {
             football fan, and I dream of traveling the world, discovering new
             adventures and flavors along the way. This website is my little
             corner of the internet where I share my journey, passions, and
-            everything that makes me, well, me. Welcome to my world!
+            everything that makes me, well, me!
           </Typography>
         </Grid>
       </Grid>
@@ -162,7 +185,6 @@ export default function Home() {
         }}
       >
         <Typography
-          ref={galleryRef}
           sx={{
             fontSize: "34px",
             fontWeight: 500,
@@ -198,7 +220,213 @@ export default function Home() {
           />
         </Box>
       </Grid>
-      <Box ref={placesRef}>travell</Box>
+      <Grid
+        container
+        sx={{
+          // height: "500px",
+
+          padding: "20px",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Typography
+          sx={{
+            fontSize: "34px",
+            fontWeight: 500,
+            textTransform: "uppercase",
+
+            textAlign: "center",
+          }}
+        >
+          Travel Dairy
+        </Typography>
+        <Box
+          sx={{
+            display: "grid",
+            justifyContent:"center",
+            alignContent:"center",
+            // columnGap: { xs: "2rem", md: "1rem", lg: "2rem", xl: "3rem" },
+            // rowGap: { xs: "2rem", lg: "1rem" },
+            gridTemplateColumns: {
+              xs: "repeat(4, 1fr)",
+              sm: "repeat(2, 1fr)",
+              lg: "repeat(3, 1fr)",
+            },
+            gridTemplateRows: "repeat(1, 120px)",
+          }}
+        >
+          {avatarData.map((item) => (
+          <Box
+          key={item?.id}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignContent: "center",
+              width: "fit-content",
+            }}
+          >
+            <Avatar
+              alt="ss"
+               src={item?.img}
+               onClick={() => handleClickOpen(item?.label)}
+              sx={{ width: 56, height: 56, cursor: "pointer" }}
+            />
+            <Typography sx={{ textAlign: "center" }}>{item?.label}</Typography>
+          </Box>
+         ))}
+         
+        </Box>
+      </Grid>
+      <Dialog
+        maxWidth="xl"
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+        {dailogeTitle?dailogeTitle:""}
+        </DialogTitle>
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={{
+            position: "absolute",
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
+          }}
+        >
+          <CloseIcon />
+        </IconButton>
+        <DialogContent
+          sx={{
+            overflowY: "hidden",
+          }}
+          dividers
+        >
+          <ImageList
+            sx={{ width: 300, height: 450, overflowY: "scroll",
+            overflowX: "none",
+            "&::-webkit-scrollbar": {
+              display: "none",
+            }, }}
+            variant="quilted"
+            cols={4}
+            rowHeight={121}
+          >
+            {itemData.map((item) => (
+              <ImageListItem
+                key={item.img}
+                cols={item.cols || 1}
+                rows={item.rows || 1}
+              >
+                <img
+                  {...srcset(item.img, 121, item.rows, item.cols)}
+                  alt={item.title}
+                  loading="lazy"
+                />
+              </ImageListItem>
+            ))}
+          </ImageList>
+        </DialogContent>
+      </Dialog>
     </Grid>
   );
 }
+const avatarData = [
+  {
+    id:1,
+    label:"Morocco",
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+  },
+  {
+    id:2,
+    label:"Scotland",
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+  },
+  {
+    id:3,
+    label:"Turkey",
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+  },
+  {
+    id:4,
+    label:"Brighton",
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+  },
+  {
+    id:5,
+    label:"Wales",
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+  },
+ 
+  {
+    id:8,
+    label:"London",
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+  },
+ 
+ 
+];
+const itemData = [
+  {
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    title: "Burger",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    title: "Camera",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    title: "Coffee",
+    cols: 2,
+  },
+  {
+    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+    title: "Hats",
+    cols: 1,
+  },
+  {
+    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+    title: "Honey",
+    author: "@arwinneil",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+    title: "Basketball",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+    title: "Fern",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+    title: "Mushrooms",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+    title: "Tomato basil",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+    title: "Sea star",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1589118949245-7d38baf380d6",
+    title: "Bike",
+    cols: 2,
+  },
+];
